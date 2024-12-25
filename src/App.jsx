5c0dce45a0dcb7997b2b52b26be2e8fe97808137
Polas Chandra Barmon <dpi.polas@gmail.com>
2024-12-25 14:58:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true,
+      }}
+    >
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
