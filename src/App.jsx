abf85c33231eb2b25d250d6b947bcf8449930a27
Polas Chandra Barmon <dpi.polas@gmail.com>
2024-12-26 20:45:44 +0600

    added some pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,26 @@
-function Homepage() {
-  return <div>World Wise</div>;
-}
+import { Link } from "react-router";
+import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
-export default Homepage;
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link className="cta" to="/app">
+          Let Start Tracking
+        </Link>
+      </section>
+    </main>
+  );
+}
